c8e2a2a8d5635f3768a0a76f97c937490c3b04e8
Kirill Udaltsov <lillinearlight@gmail.com>
2019-07-20 09:02:54 +0600

    Updates

---
+++ precache-manifest.9bac1767e2a73b43d764ac47b1ea97f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9d98a13e9fde145a0c31c7a93548f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdefad2ccd9f4e4a4985",
+    "url": "/static/css/main.e81b5d39.chunk.css"
+  },
+  {
+    "revision": "d4e7e21eef418568a80a",
+    "url": "/static/js/2.e41e6091.chunk.js"
+  },
+  {
+    "revision": "fdefad2ccd9f4e4a4985",
+    "url": "/static/js/main.92c98d83.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "43c0fcd4e2811f7c13df3e63f8200087",
+    "url": "/static/media/01-3.43c0fcd4.jpg"
+  },
+  {
+    "revision": "8f1d95c883dd8a12d47d0a8ac5115373",
+    "url": "/static/media/forklift-835340_1920.8f1d95c8.jpg"
+  },
+  {
+    "revision": "9c812845746c5637fd0be088bcaf2e11",
+    "url": "/static/media/laptop-3190194_1920.9c812845.jpg"
+  },
+  {
+    "revision": "49bd966b6b0b39be6414cb3bf2d3bf29",
+    "url": "/static/media/mosque-615415_1920.49bd966b.jpg"
+  },
+  {
+    "revision": "31fc08a1173b9481359647b8bb453945",
+    "url": "/static/media/safe-913452_1920.31fc08a1.jpg"
+  }
+]);
